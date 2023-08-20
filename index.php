87d780798684551fccd55a9681e5570ac63b06b5
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2023-08-20 14:59:45 -0400

    fonts

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     <div id="content">
         <p class="hidden" style="height: fit-content; width:fit-content;">Welcome to Eternaments, the premier online tournament hosting platform for gamers around the world! We are a company that is dedicated to providing high-quality and engaging gaming experiences for our community.</p>
         <div id="firstImage" class="imgHolder">
-            <p class="hidden flexItems" style="height: fit-content; text-align: left; width: 50%;">At Eternaments, we believe that gaming is more than just a hobby - it’s a way of life. We understand that the gaming community is a passionate and dedicated group of individuals who are always looking for new challenges and opportunities to showcase their skills.</p>
+            <p class="hidden flexItems paragraphs" style="height: fit-content; text-align: left; width: 50%;">At Eternaments, we believe that gaming is more than just a hobby - it’s a way of life. We understand that the gaming community is a passionate and dedicated group of individuals who are always looking for new challenges and opportunities to showcase their skills.</p>
             <div style="width: 50%;" class="flexItems imageholders">
                 <div id="firstImageimg" class="hidden flexItems image"></div>
                 
@@ -63,14 +63,14 @@
                 <div id="secondImageimg" class="hidden flexItems image"></div>
                 
             </div>
-            <p class="hidden flexItems " style="height: fit-content; text-align: right; width: 50%;">That’s why we have created a platform that is designed to cater to the needs of gamers of all skill levels. Whether you are a casual player or a professional gamer, we have tournaments that are tailored to meet your needs. We host a wide range of tournaments for popular games such as Valorant, Overwatch, CSGO, and many more. Each tournament comes with exciting prizes for the winners, giving you the motivation to put your best foot forward.</p>
+            <p class="hidden flexItems paragraphs " style="height: fit-content; text-align: right; width: 50%;">That’s why we have created a platform that is designed to cater to the needs of gamers of all skill levels. Whether you are a casual player or a professional gamer, we have tournaments that are tailored to meet your needs. We host a wide range of tournaments for popular games such as Valorant, Overwatch, CSGO, and many more. Each tournament comes with exciting prizes for the winners, giving you the motivation to put your best foot forward.</p>
            
             
         </div>
         <br>
         <div id="thirdImage" class="imgHolder">
             
-            <p class="hidden flexItems" style="height: fit-content; text-align: left; width: 50%;">At Eternaments, we take pride in the quality of our tournaments. We work tirelessly to ensure that each event is well-organized, fair, and enjoyable for everyone involved. Our team of experienced tournament organizers is always on hand to answer any questions you may have and provide you with the support you need to succeed.
+            <p class="hidden flexItems paragraphs" style="height: fit-content; text-align: left; width: 50%;">At Eternaments, we take pride in the quality of our tournaments. We work tirelessly to ensure that each event is well-organized, fair, and enjoyable for everyone involved. Our team of experienced tournament organizers is always on hand to answer any questions you may have and provide you with the support you need to succeed.
             </p>
             <div style="width: 50%;" class="flexItems imageholders">
                 <div id="thirdImageimg" class="hidden flexItems image"></div>
@@ -84,7 +84,7 @@
                 <div id="fourthImageimg" class="hidden flexItems image"></div>
                 
             </div>
-            <p class="hidden flexItems" style="height: fit-content; text-align: right; width: 50%;">We are passionate about gaming, and we are committed to fostering a community that is inclusive, welcoming, and supportive. We believe that gaming is a powerful tool that can bring people together and create meaningful connections.
+            <p class="hidden flexItems paragraphs" style="height: fit-content; text-align: right; width: 50%;">We are passionate about gaming, and we are committed to fostering a community that is inclusive, welcoming, and supportive. We believe that gaming is a powerful tool that can bring people together and create meaningful connections.
             </p>
             
             
