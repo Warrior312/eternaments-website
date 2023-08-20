483573bb1d6cf38174d08d933bd1bdade9f70854
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2023-08-20 12:48:37 -0400

    adding mobile layouts for HOF

--- pages/comingsoon.php
+++ pages/comingsoon.php
@@ -4,6 +4,7 @@
     ?>
     <link rel="stylesheet" href="cs.css">
     <body>
+        <meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=1" />
         <div id="container">
         <div id="background"></div>
         <div id="text">This sector hasn't been built yet.
