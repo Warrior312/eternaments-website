269aaae18958405eb0c9aadeadb02af4b552274a
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2023-08-21 11:11:50 -0400

    adding bot toggleables

---
+++ includes/maint.inc.php
@@ -0,0 +1,26 @@
+<?php
+    require_once 'dbh.inc.php';
+    
+    $sql = "SELECT * FROM sys";
+    
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+       
+       exit();
+    }else{
+        mysqli_stmt_execute($stmt);
+        $rows = mysqli_stmt_get_result($stmt);
+        $rows = mysqli_fetch_all($rows);
+        
+        if($rows[0][1] == 1){
+            
+            header("location: ../maintenance.php");
+            print_r("Moving");
+            exit();
+        }
+    }
+
+
+
+
+?>
