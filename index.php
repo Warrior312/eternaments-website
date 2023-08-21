5409e98badeac60d21f88a979eb746315079fd0d
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2023-08-21 10:45:57 -0400

    adding bot toggleables

--- index.php
+++ index.php
@@ -27,7 +27,8 @@
         $rows = mysqli_stmt_get_result($stmt);
         $rows = mysqli_fetch_all($rows);
         print_r($rows);
-        if($rows[0][1] === 1){
+        if($rows[0][1] == 1){
+            print_r("Moving");
             header("location: /maintenance.php");
             exit();
         }
