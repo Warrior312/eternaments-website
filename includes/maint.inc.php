5e018ce6be1b382d8b83ed51e60c43361efdf141
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2023-08-21 11:16:36 -0400

    adding bot toggleables

---
+++ includes/maint.inc.php
@@ -0,0 +1,25 @@
+<?php 
+    
+    
+    require_once 'includes/dbh.inc.php';
+    
+    $sql = "SELECT * FROM sys";
+    
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+       
+       exit();
+    }else{
+        mysqli_stmt_execute($stmt);
+        $rows = mysqli_stmt_get_result($stmt);
+        $rows = mysqli_fetch_all($rows);
+        
+        if($rows[0][1] == 1){
+            
+            header("location: https://eternaments.com/maintenance.php");
+            
+            exit();
+        }
+    }
+    
+?>
--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<?php 
-    
-    
-    require_once 'includes/dbh.inc.php';
-    
-    $sql = "SELECT * FROM sys";
-    
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)){
-       
-       exit();
-    }else{
-        mysqli_stmt_execute($stmt);
-        $rows = mysqli_stmt_get_result($stmt);
-        $rows = mysqli_fetch_all($rows);
-        
-        if($rows[0][1] == 1){
-            
-            header("location: maintenance.php");
-            print_r("Moving");
-            exit();
-        }
-    }
-    
+<?php
+
+    require_once 'includes/maint.inc.php'
 ?>
 <link rel="stylesheet" href="style.css">
 <html>
