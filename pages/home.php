1479ea3a177ce09d7b77eb52ef50d6d4961e7e04
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2023-10-26 21:27:58 -0400

    editing improper urls

--- pages/home.php
+++ pages/home.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=1" />
     
     <title>Eternaments</title>
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
