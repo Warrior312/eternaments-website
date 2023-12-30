21193e22191129b8e5939b6b5079afc20c285e29
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2023-12-30 14:00:02 -0500

    changing some files...

--- includes/maint.inc.php
+++ includes/maint.inc.php
@@ -1,21 +1,17 @@
 <?php
 
-// require_once 'dbh.inc.php';
-
-// $sql = "SELECT * FROM sys";
-
-// $stmt = mysqli_stmt_init($conn);
-// if (!mysqli_stmt_prepare($stmt, $sql)) {
-//     exit();
-// } else {
-//     mysqli_stmt_execute($stmt);
-//     $rows = mysqli_stmt_get_result($stmt);
-//     $rows = mysqli_fetch_all($rows);
-
-//     if ($rows[0][1] == 1) {
-//         header("location: https://eternaments.com/pages/maintenance.php");
-//         exit();
-//     }
-// }
-
+ require_once 'dbh.inc.php';
+ $sql = "SELECT * FROM sys";
+ $stmt = mysqli_stmt_init($conn);
+ if (!mysqli_stmt_prepare($stmt, $sql)) {
+     exit();
+ } else {
+     mysqli_stmt_execute($stmt);
+     $rows = mysqli_stmt_get_result($stmt);
+     $rows = mysqli_fetch_all($rows);
+     if ($rows[0][1] == 1) {
+         header("location: https://eternaments.com/pages/maintenance.php");
+         exit();
+     }
+ }
 ?>
