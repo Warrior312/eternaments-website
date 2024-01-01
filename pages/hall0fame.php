d04a497818c26d0c9844d13e45d61a18bf4d82f2
Warrior312 <87901386+Warrior312@users.noreply.github.com>
2024-01-01 12:22:54 -0500

    adding error reporting to the hall of fame page for debug

--- pages/hall0fame.php
+++ pages/hall0fame.php
@@ -14,6 +14,7 @@
 
     <div id="honorsBG">
         <?php
+        error_reporting(E_ALL | E_STRICT);
         // this PHP code checks for connection errors to the discord channel.
         if (isset($_SESSION["connErr"])) {
             print_r($_SESSION["connErr"]);
